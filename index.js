d9e3dae8c4b1bea68ba824de8c43b2290eff2d16
arshlaanhaq <arshlaanhaq005@.com>
2023-01-27 23:17:03 +0530

    second commit

--- index.js
+++ index.js
@@ -3,3 +3,11 @@
 alert(`hello buddy!!!
 this site is made for display purpose only!   
 Have Fun :)`);
+
+let docTitle = document.title;
+window.addEventListener("blur", ()=>{
+    document.title ="Come back :(";
+})
+window.addEventListener("focus",()=>{
+    document.title= docTitle;
+})
